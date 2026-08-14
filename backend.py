003f70623ac0ab76a9f83faf08dc0f38b5bff5f5
aniket902223 <it@magnetontech.com>
2026-08-14 15:11:51 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from spark.sql import *
 from spark.type import *
-        "client_secret": "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",
+        "client_secret": "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",
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",

49108d55481bcd413d71d643682ab1b27b23c1ee
aniket902223 <it@magnetontech.com>
2026-08-14 14:43:08 +0530

    Fix formatting and remove extra whitespace in backend.py

--- backend.py
+++ backend.py
@@ -4,9 +4,9 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from spark.sql import *
 from spark.type import *
-
         "client_secret": "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",
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
         "redirect_uris": ["http://localhost"]
+

794f6464eaba824b3dacf0a23384d6b6b1df2f1d
aniket902223 <it@magnetontech.com>
2026-08-14 13:58:12 +0530

    Import Spark SQL and types in backend.pysecond commit

--- backend.py
+++ backend.py
@@ -2,3 +2,5 @@ import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from spark.sql import *
+from spark.type import *
